00a9b4384506b0d83080a759f69a205cbf40c315
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2021-03-16 23:04:05 -0500

    Ajustes para crear un jar o war

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # CRUD Student
 Ejemplo de crud usando java (Spring Boot).
 
-Funcional con JDK 11
+Funcional con JDK 11, está preparado para generar tanto un JAR (por defecto) como un WAR
+
+**Para generar JAR**
+
+    mvn clean package
+o
+
+    mvn clean package -Pjar
+
+**Para generar WAR**
+
+    mvn clean package  -Pwar
--- src/main/java/com/lmalvarez/demo/StudentsDemoApplication.java
+++ src/main/java/com/lmalvarez/demo/StudentsDemoApplication.java
@@ -2,9 +2,15 @@ package com.lmalvarez.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentsDemoApplication {
+public class StudentsDemoApplication extends SpringBootServletInitializer{
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StudentsDemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsDemoApplication.class, args);
