6acec6f5f8a064ea5edf07223b004060bc945a70
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2021-03-16 21:44:42 -0500

    Version inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# CRUD Student
+Ejemplo de crud usando java (Spring Boot).
+
+Funcional con JDK 11
---
+++ src/main/java/com/lmalvarez/demo/student/StudentConfig.java
@@ -0,0 +1,23 @@
+package com.lmalvarez.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+			Student mariam = new Student("Mariam", "ma@gmail.com", LocalDate.of(2000, Month.JANUARY, 2));
+			
+			Student alex = new Student("Alex", "al@gmail.com", LocalDate.of(2003, Month.AUGUST, 3));
+			
+			studentRepository.saveAll(List.of(mariam, alex));
+		};
+	}
+}
